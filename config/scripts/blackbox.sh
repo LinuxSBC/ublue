9e1127f5487c0e39e743b1de0e3b26f50c00344b
LinuxSBC <benedict.p.simmons@gmail.com>
2023-10-28 19:41:55 -0500

    (WIP) Set BlackBox to default

---
+++ config/scripts/blackbox.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -oue pipefail
+
+echo "-- Removing gnome-terminal in favor of the BlackBox flatpak --"
+rpm-ostree override remove gnome-terminal gnome-terminal-nautilus
+
+echo "-- Installing OpenInBlackBox for Nautilus integration --"
+if [[ ! -d /usr/share/nautilus-python/extensions/ ]]; then
+    mkdir -v -p /usr/share/nautilus-python/extensions/
+fi
+curl https://raw.githubusercontent.com/ppvan/nautilus-open-in-blackbox/main/nautilus-open-in-blackbox.py > /usr/share/nautilus-python/extensions/blackbox_extension.py
+
+echo "-- Setting BlackBox as default terminal --"
+update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/blackbox-terminal 50
+update-alternatives --set x-terminal-emulator /usr/bin/blackbox-terminal
+
+echo "X-ExecArg=-c" >> /usr/share/applications/com.raggesilver.BlackBox.desktop
+curl https://raw.githubusercontent.com/Vladimir-csp/xdg-terminal-exec/master/xdg-terminal-exec > /usr/bin/xdg-terminal-exec
+chmod +x /usr/bin/xdg-terminal-exec
