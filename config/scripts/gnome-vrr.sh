e93baf7a12f1612c3c6af6ae19201bf9b18be707
LinuxSBC <benedict.p.simmons@gmail.com>
2024-01-23 20:21:26 -0600

    Hopefully fix gnome-vrr script

---
+++ config/scripts/gnome-vrr.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -euo pipefail
+
+wget -O "/etc/yum.repos.d/_copr_kylegospo-gnome-vrr.repo" "https://copr.fedorainfracloud.org/coprs/kylegospo/gnome-vrr/repo/fedora-${OS_VERSION}/kylegospo-gnome-vrr-fedora-${OS_VERSION}.repo"
+
+rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:kylegospo:gnome-vrr mutter mutter-common gnome-control-center gnome-control-center-filesystem
+
+rm -f /etc/yum.repos.d/_copr_kylegospo-gnome-vrr.repo
